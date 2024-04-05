a396cb117c42f32b250fd006b36a412f0586279d
Tracy Chimita <nellyachieng88@gmail.com>
2024-04-05 11:51:42 +0300

    modifying-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+const yourName = "Nelly Ochino" 
+newHeader.textContent = ${yourName} champion;
+newHeader.className = "victory";
+document.body.append(newHeader);
